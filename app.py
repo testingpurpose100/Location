c9b6350908ec99463ee5e84629b9961612a95a88
testingpurpose100 <testingpurposeonly100@gmail.com>
2025-04-26 16:53:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,5 +103,8 @@ def get_place_description(place):
 
     return ", ".join(description)
 
+import os
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Get PORT from environment
+    app.run(host='0.0.0.0', port=port, debug=True)
